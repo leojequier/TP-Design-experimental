93ca675d3c61008a31ff90dd5ff88a0aaa0098ed
Olivier <Bidabinch@users.noreply.github.com>
2017-10-19 12:18:08 +0200

    scale change

--- Data_csv/Week1.R
+++ Data_csv/Week1.R
@@ -269,13 +269,10 @@ SCTxmixC<-(c(SCT1mixC,SCT2mixC,SCT3mixC,SCT4mixC,SCT5mixC,SCT6mixC,SCT7mixC,SCT8
 PPSCTxTol<-(c(PPSCTol.T1,PPSCTol.T2,PPSCTol.T3,PPSCTol.T4,PPSCTol.T5,PPSCTol.T6,PPSCTol.T7,PPSCTol.T8))
 PPSCTxmixC<-(c(PPSCmixC.T1,PPSCmixC.T2,PPSCmixC.T3,PPSCmixC.T4,PPSCmixC.T5,PPSCmixC.T6,PPSCmixC.T7,PPSCmixC.T8))
 
-plot(SCTxmixC, col="black")
-points(SCTxTol, col="red")
-points(PPSCTxmixC, col="blue")
-points(PPSCTxTol, col="orange")
+par(mfrow = c(1,1))
 
 tot <- data.frame(time, SCTxmixC, SCTxTol, PPSCTxmixC, PPSCTxTol)
-plot(tot$time, tot$SCTxmixC,log="y",ylim=c(min(tot$PPSCTxTol), max(tot$PPSCTxTol)), type="o",main="Compte de SC",xlab= "Heures", ylab=" Nombre de cellules")
+plot(tot$time, tot$SCTxmixC,log="y",ylim=c(min(tot$PPSCTxTol), max(tot$PPSCTxTol)), type="o",main="SC growth",xlab= "Heures", ylab="log(SC count)")
 points(tot$time,tot$SCTxTol, type="o", col="red")
 points(tot$time,tot$PPSCTxmixC, type="o", col="blue")
 points(tot$time,tot$PPSCTxTol, type="o", col="orange")
